77259b1ffe3b8f13e110ca898a05c841b152d876
DAVIDLAP\David <davidcnns@gmail.com>
2016-10-16 19:35:42 +0200

    no message

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
-from Distribucions import uniforme_discreta
+from Distributions import uniforme_discreta
+from Distributions import bernoulli
+
+def seguentVehicle():
+    return bernoulli(.15, 'moto', 'cotxe')
+    
+def tempsDipositMoto():
+    return erlang2(3)
+    
+def erlang2(u):
+    
+    
