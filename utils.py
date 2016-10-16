6c98d3c77339e59ea9ae8a94362a895baf376936
DAVIDLAP\David <davidcnns@gmail.com>
2016-10-16 19:55:05 +0200

    Added normalTrunc function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
-from Distributions import uniforme_discreta
-from Distributions import bernoulli
+from Distribucions import uniforme_discreta
+from Distribucions import bernoulli
+import numpy
 
 def seguentVehicle():
     return bernoulli(.15, 'moto', 'cotxe')
@@ -7,6 +8,20 @@ def seguentVehicle():
 def tempsDipositMoto():
     return erlang2(3)
     
+def tempsDipositCotxe():
+    return 
+    
 def erlang2(u):
     
+    return 0
     
+def normalTrunc(u, sig, vmin, vmax):
+    randomVal = numpy.random.normal(u, sig)
+    count = 0
+    while count < 1000000:
+        if vmin <= randomVal <= vmax:
+            return randomVal
+        else:
+            randomVal = numpy.random.normal(u, sig)
+        count += 1
+    return u
